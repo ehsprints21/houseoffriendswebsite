b9f414fcde40f6ebfdc4f6dd6347581fbb90f0bc
unknown <you@example.com>
2022-06-25 16:48:15 +0530

    Submission 1.1

--- node-api/app.js
+++ node-api/app.js
@@ -18,7 +18,8 @@ const mongoose = require('mongoose');
 
 dotenv.config();
 
-mongoose.connect('mongodb://localhost:27017/lonerDB');
+mongoose.connect(`${process.env.MONGO_URI}`, { useNewUrlParser: true });
+
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
