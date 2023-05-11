4575d77708d1b787ee68698b50d61d78070262e6
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-05-11 17:37:08 +0600

    Production link added

--- client/src/Axios/requestMethods.js
+++ client/src/Axios/requestMethods.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:4000/api/";
+// const BASE_URL = "http://localhost:4000/api/";
+const BASE_URL = "https://sindrisystems.com:4000 "; // for production
 
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
@@ -10,13 +11,13 @@ const currentUser = user && JSON.parse(user).currentUser;
 //console.log(currentUser);
 const TOKEN = currentUser?.accessToken;
 
- const publicRequest = axios.create({
+const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
 
- const userRequest = axios.create({
+const userRequest = axios.create({
   baseURL: BASE_URL,
   headers: { token: TOKEN },
 });
 
-export {publicRequest, userRequest}
+export { publicRequest, userRequest }
